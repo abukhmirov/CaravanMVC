4086903df6014e2f0dc6492d89d9a3e12e268154
abukhmirov <alexbukhmirov@gmail.com>
2023-08-21 14:37:26 -0600

    created wagon Index action and view

---
+++ CaravanMVC/Controllers/WagonsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
+
+namespace CaravanMVC.Controllers
+{
+    public class WagonsController : Controller
+    {
+        private readonly CaravanMvcContext _context;
+
+        public WagonsController(CaravanMvcContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var wagons = _context.Wagons.ToList();
+            return View(wagons);
+        }
+        
+        
+    }
+}
