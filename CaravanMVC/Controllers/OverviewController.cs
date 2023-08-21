677b8825bca972cf17b8cd33a3a766312b1a091a
abukhmirov <alexbukhmirov@gmail.com>
2023-08-21 15:56:27 -0600

    fixed overview list

---
+++ CaravanMVC/Controllers/OverviewController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
+
+namespace CaravanMVC.Controllers
+{
+    public class OverviewController : Controller
+    {
+        private readonly CaravanMvcContext _context;
+
+        public OverviewController(CaravanMvcContext context)
+        {
+            _context = context;
+        }
+
+
+        [Route("/overview")]
+        public IActionResult Show()
+        {
+            var passengers = _context.Passengers;
+            
+            List<string> Destinations = new List<string>();
+
+            foreach (var passenger in passengers)
+            {
+                Destinations.Add(passenger.Destination);
+            }
+            ViewBag.Destinations = Destinations;
+            return View();
+        }
+
+
+    }
+}
